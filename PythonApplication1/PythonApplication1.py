cc024c617d767c5fa1040f345791f0e0fef7d221
ZGL66666666 <101685050+ZGL66666666@users.noreply.github.com>
2024-04-24 07:04:47 +0800

    Update PythonApplication1.py

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -40,3 +40,14 @@ for n in range(2,50):  #  s != 0  t != m*m*m  Any cube  and Any number of cubes 
     s = (n-1)**3 - (n**3 - (n-1)**3)
     #if s > 0:
     print(n,n**3,(n-1)**3,t,s)
+    
+n=np.arange(0.0,6.0,0.001)
+t=np.sqrt(2*n-1) #(2*n-1)**(1/2)
+t1=(3*n*n-3*n+1)**(1/3)
+#s=n*n*n-6*n*n+6*n-2
+#plt.plot(n,s)
+plt.plot(n,t)
+plt.plot(n,t1)
+plt.plot([0,6],[0,0])
+plt.grid()
+plt.show()
